e5bf68f366b663074b0139d2e8e47260b2792b2c
Josh Lawson <joshlawson100@outlook.com>
2025-11-27 17:27:26 +1100

    Create placeholder frontent routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,11 +8,35 @@ import usersRouter from "./api/users";
 
 export const register = (app: express.Application) => {
   app.get("/", (req, res) => {
-    res.render("index");
+    res.render("index", { title: "Music App", content: "pages/home" });
   });
 
-  app.get("/about", (req, res) => {
-    res.render("about");
+  app.get("/explore", (req, res) => {
+    res.render("index", { title: "Music App", content: "pages/explore" });
+  });
+
+  app.get("/songs", (req, res) => {
+    res.render("index", { title: "Music App", content: "pages/songs" });
+  });
+
+  app.get("/artists", (req, res) => {
+    res.render("index", { title: "Music App", content: "pages/artists" });
+  });
+
+  app.get("/albums", (req, res) => {
+    res.render("index", { title: "Music App", content: "pages/albums" });
+  });
+
+  app.get("/playlists", (req, res) => {
+    res.render("index", { title: "Music App", content: "pages/playlists" });
+  });
+
+  app.get("/liked", (req, res) => {
+    res.render("index", { title: "Music App", content: "pages/liked" });
+  });
+
+  app.get("/episodes", (req, res) => {
+    res.render("index", { title: "Music App", content: "pages/episodes" });
   });
 
   app.use("/api/albums", albumsRouter);
